5bf55a45d9574fe8f46334e76b84b17af2a93b7e
Alisher <alik20021223@gmail.com>
2024-05-22 14:24:53 +0500

    fix some bugs

--- src/pages/Vacancy/VacancyDescribe.tsx
+++ src/pages/Vacancy/VacancyDescribe.tsx
@@ -6,6 +6,8 @@ import { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import api from "../../api";
 import { BlockVacancyType } from "./_components/type";
+import { useDispatch } from "react-redux";
+import { setLoadingPage } from "../../state/pagesSlice";
 
 
 export const VacancyDescribe = () => {
@@ -15,57 +17,56 @@ export const VacancyDescribe = () => {
     const [isRecom, setRecom] = useState<BlockVacancyType[]>([])
 
 
-
+    const dispatch = useDispatch()
 
 
     useEffect(() => {
         const fetchData = async () => {
             try {
+                dispatch(setLoadingPage(true))
                 const res = await api.get(`vacancies/${slug}`);
                 setData(res.data);
 
                 const formData = {
                     industryId: res.data.industryId,
-                };                
+                };
 
                 const response = await api.post(`/vacancies/per-page/20/filter`, formData);
 
-                const recom  = response.data.data.filter((item: BlockVacancyType) => item.slug != slug)
-                
+                const recom = response.data.data.filter((item: BlockVacancyType) => item.slug != slug)
+
                 setRecom(recom);
             } catch (error) {
                 console.error('There was an error!', error);
+            } finally {
+                dispatch(setLoadingPage(false))
             }
         };
 
         fetchData();
     }, []);
 
-    
+
 
     const { t } = useTranslation();
 
 
-    if (isData) {
-        return (
-            <div className="container m-auto sm:px-5 max-sm:px-5 w-full">
-                <div className="flex max-md:flex-col max-md:space-y-5  justify-between">
-                    <div className="md:w-[68%] max-md:w-full">
-                        <DescribeVacancy data={isData} />
-                    </div>
-                    <div className="w-[30%] max-md:w-full">
-                        <RespondBlock city={isData.cityId} money={isData.price} schedules={isData.scheduleId} experiences={isData.industryId} />
-                    </div>
+    return (
+        isData && <div className="container m-auto sm:px-5 max-sm:px-5 w-full">
+            <div className="flex max-md:flex-col max-md:space-y-5  justify-between">
+                <div className="md:w-[68%] max-md:w-full">
+                    <DescribeVacancy data={isData} />
                 </div>
-                <div className="mt-[60px]">
-                    <h1 className="font-bold text-3xl">{t('SimilarVacancies')}</h1>
-                    <div className="mt-10">
-                        <RecomVacancy data={isRecom} />
-                    </div>
+                <div className="w-[30%] max-md:w-full">
+                    <RespondBlock city={isData.cityId} money={isData.price} schedules={isData.scheduleId} experiences={isData.industryId} />
                 </div>
             </div>
-        );
-    } else {
-        return <div>Вакансия не найдена</div>;
-    }
+            <div className="mt-[60px]">
+                <h1 className="font-bold text-3xl">{t('SimilarVacancies')}</h1>
+                <div className="mt-10">
+                    <RecomVacancy data={isRecom} />
+                </div>
+            </div>
+        </div>
+    );
 }
